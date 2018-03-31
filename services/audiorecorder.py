53950d76222285a098db8fc2fd0dc2a5157a8be9
Thomas Berdy <tberdy@hotmail.fr>
2018-03-31 12:29:57 +0200

    - audiorecorder

--- services/audiorecorder.py
+++ services/audiorecorder.py
@@ -1,23 +1,48 @@
 from threading import Thread
 import sounddevice as sd
 import soundfile as sf
+import queue
+import uuid
+import logging
 
 
 class AudioRecorder(Thread):
-    def __init__(self, duration):
+    def __init__(self, samplerate=44100, channels=1):
         Thread.__init__(self)
 
-        self.duration = duration
-        self.samplerate = 44100
-        self.channels = 1
-        self.output_file = "tmp/record.wav"
+        self.samplerate = samplerate
+        self.channels = channels
+
+        self.q = queue.Queue()
+
+        logging.basicConfig(level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
 
     def run(self):
-        record = sd.rec(
-            int(self.duration * self.samplerate),
-            samplerate=self.samplerate,
-            channels=self.channels
-        )
+        while True:
+            elm = self.q.get()
+            self.logger.info('Now recording for {}'.format(elm['duration']))
 
-        sf.write(self.output_file, record, self.samplerate)
-        sd.play(record, self.samplerate)
+            record = sd.rec(
+                int(elm['duration'] * self.samplerate),
+                samplerate=self.samplerate,
+                channels=self.channels,
+                blocking=True
+            )
+            sf.write(elm['filename'], record, self.samplerate)
+
+            self.logger.info(
+                'Record ended, launching {}'.format(elm['callback'].__name__)
+            )
+            elm['callback']()
+
+    def record(self, duration, callback):
+        elm = {}
+        elm['duration'] = duration
+        elm['filename'] = 'tmp/{}.wav'.format(uuid.uuid4())
+        elm['callback'] = callback
+        self.q.put(elm)
+
+        self.logger.info(
+            'Adding a {} seconds record to queue'.format(elm['duration'])
+        )

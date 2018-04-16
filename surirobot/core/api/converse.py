9fd1785f2c312bb55d21fbace9386769c15a37e2
Alain Berrier <alain.berrier@outlook.com>
2018-04-16 10:41:06 +0200

    fix and blind

--- surirobot/core/api/converse.py
+++ surirobot/core/api/converse.py
@@ -34,7 +34,7 @@ class ConverseApiCaller(ApiCaller):
         self.isBusy = False
         buffer = QByteArray(reply.readAll())
         if (reply.error() != QNetworkReply.NoError):
-            print("Error  " + str(reply.error()) + " : " + buffer.data().decode('utf8'))
+            print("NLP - Error  " + str(reply.error()) + " : " + buffer.data().decode('utf8'))
             self.intent = "message"
             self.message = "Il semblerait qu'il y ait eu un problème"
             self.download.emit("https://i1.theportalwiki.net/img/d/d0/GLaDOS_potatos_sp_a4_intro_uhoh03_fr.wav")

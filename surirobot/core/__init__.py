645bf61efb3007dfa67a91c57bd25dc9839c6cf2
Alain Berrier <alain.berrier@outlook.com>
2018-04-25 11:55:03 +0200

    fix

--- surirobot/core/__init__.py
+++ surirobot/core/__init__.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from .gui.ui import MainWindow
-from .gui.test import Ui_MainWindow
+from .gui.new import Ui_MainWindow
 
 # Load QT
 app = QApplication(sys.argv)

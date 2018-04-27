350f10c0aaa5990dd926ff59d05082cb3e136ebe
Alain Berrier <alain.berrier@outlook.com>
2018-04-27 23:24:27 +0200

    deployment script

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pyinstaller start.py
-cp -rf /usr/local/lib/python3.5/dist-packages/face_recognition_models ${DIR}/dist/start/
-cp -rf res ${DIR}/dist/start/
-cp -rf data ${DIR}/dist/start/
-mkdir ${DIR}/dist/start/tmp
-cp -rf scenario.json ${DIR}/dist/start/scenario.json
-cp -rf .env ${DIR}/dist/start/.env

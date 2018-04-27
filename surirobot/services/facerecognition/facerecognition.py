ee713095c124a23bdc3f4ee85a5a443924ad4911
Alain Berrier <alain.berrier@outlook.com>
2018-04-27 15:24:34 +0200

    Great improvments in face recognition !

--- surirobot/services/facerecognition/facerecognition.py
+++ surirobot/services/facerecognition/facerecognition.py
@@ -1,18 +1,17 @@
-from PyQt5.QtCore import QThread, pyqtSlot, pyqtSignal
+from PyQt5.QtCore import QThread, pyqtSlot, pyqtSignal, QTimer
 import os
 import logging
 import time
 import face_recognition
 from surirobot.core import ui
 from surirobot.services import serv_vc
-from .counter import Counter
 from surirobot.core.common import State
 
 
 class FaceRecognition(QThread):
     updateState = pyqtSignal(str, int, dict)
     # deprecated signal -> will be removed
-    person_changed = pyqtSignal(str)
+    signalPersonChanged = pyqtSignal(str)
 
     NB_IMG_PER_SECOND = 2
     MODULE_NAME = 'face'
@@ -31,17 +30,29 @@ class FaceRecognition(QThread):
         logging.basicConfig(level=logging.INFO)
         self.logger = logging.getLogger(__name__)
 
-        self.person_changed.connect(ui.setTextUp)
+        self.signalPersonChanged.connect(ui.setTextUp)
 
         self.data = {}
         self.faces = []
         self.linker = []
+        self.tolerance = float(os.environ.get('FACERECO_TOLERANCE', '0.54'))
 
-        self.counter_nobody = Counter(self.NB_IMG_PER_SECOND * 3)
-        self.counter_nobody.timeout.connect(self.timer_nobody_timeout)
-        self.counter_unknown = Counter(self.NB_IMG_PER_SECOND * 4)
+        self.faceWorkLoop = QTimer()
+        self.faceWorkLoop.timeout.connect(self.detect)
+
+        self.nobodyTimer = QTimer()
+        self.nobodyTimer.setSingleShot(True)
+        self.nobodyTimerInterval = (1000/self.NB_IMG_PER_SECOND) * 6
+        self.nobodyTimer.timeout.connect(self.nobodyTimeout)
+
+        self.counter_unknown = QTimer()
+        self.counter_unknown.setSingleShot(True)
+        self.unknownTimerInterval = (1000/self.NB_IMG_PER_SECOND) * 12
         self.counter_unknown.timeout.connect(self.timer_unknown_timeout)
-        self.counter_known = Counter(self.NB_IMG_PER_SECOND * 1)
+
+        self.counter_known = QTimer()
+        self.counter_known.setSingleShot(True)
+        self.knownTimerInterval = (1000/self.NB_IMG_PER_SECOND) * 5
         self.counter_known.timeout.connect(self.timer_known_timeout)
 
         self.state_id = self.NOBODY
@@ -55,70 +66,66 @@ class FaceRecognition(QThread):
     def __del__(self):
         self.quit()
 
-    def run(self):
-        face_locations = []
-        face_encodings = []
-        tolerance = float(os.environ.get('FACERECO_TOLERANCE', '0.54'))
-
+    def start(self):
+        QThread.start(self)
         time.sleep(3)
-
         self.emit_person_changed(self.NOBODY)
         self.emit_state_changed(self.NOBODY, self.NOBODY)
+        self.faceWorkLoop.start(1000/self.NB_IMG_PER_SECOND)
 
-        while(True):
-            time.sleep(-time.time() % (1 / self.NB_IMG_PER_SECOND))
-            small_frame = serv_vc.get_frame()
+    @pyqtSlot()
+    def detect(self):
+        face_locations = []
+        face_encodings = []
+        small_frame = serv_vc.get_frame()
 
-            # Find all the faces and face encodings in the current frame of video
-            face_locations = face_recognition.face_locations(small_frame)
-            # num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
-            face_encodings = face_recognition.face_encodings(small_frame, face_locations, 2)
-            if face_encodings:
-                if len(face_encodings) > 1:
-                    self.emit_state_changed(State.STATE_FACE_MULTIPLES, self.UNKNOWN)
-                else:
-                    # See if the face is a match for the known face(s)
-                    match = face_recognition.compare_faces(self.faces, face_encodings[0], tolerance)
-                    id = self.UNKNOWN
-                    match_tuples = list(enumerate(match))
-                    match_tuples = list(filter(lambda t: t[1], match_tuples))
-                    # print('match : ' + str(match))
-                    if len(match_tuples) == 0:
-                        self.addToBuffer(self.UNKNOWN)
-                    elif len(match_tuples) == 1:
-                        id = self.linker[match_tuples[0][0]]
-                        self.addToBuffer(id)
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(small_frame)
+        # num_jitters: How many times to re-sample the face when calculating encoding. Higher is more accurate, but slower (i.e. 100 is 100x slower)
+        face_encodings = face_recognition.face_encodings(small_frame, face_locations, 2)
+        if face_encodings:
+            if len(face_encodings) > 1:
+                self.emit_state_changed(State.STATE_FACE_MULTIPLES, self.UNKNOWN)
             else:
-                self.addToBuffer(self.NOBODY)
+                # See if the face is a match for the known face(s)
+                match = face_recognition.compare_faces(self.faces, face_encodings[0], self.tolerance)
+                id = self.UNKNOWN
+                match_tuples = list(enumerate(match))
+                match_tuples = list(filter(lambda t: t[1], match_tuples))
+                # print('match : ' + str(match))
+                if len(match_tuples) == 0:
+                    self.addToBuffer(self.UNKNOWN)
+                elif len(match_tuples) == 1:
+                    id = self.linker[match_tuples[0][0]]
+                    self.addToBuffer(id)
+        else:
+            self.addToBuffer(self.NOBODY)
 
     def addToBuffer(self, id):
-        self.counter_nobody.increment()
-        self.counter_unknown.increment()
-        self.counter_known.increment()
-
+        # Case nobody is on the camera
         if id == self.NOBODY:
             self.pretendent_id = self.NOBODY
             self.counter_unknown.stop()
             self.counter_known.stop()
-            self.emit_state_changed(State.STATE_FACE_NOT_WORKING, self.UNKNOWN)
 
-            if not self.counter_nobody.is_active():
+            if not self.nobodyTimer.isActive():
                 if self.state_id != self.NOBODY:
-                    self.counter_nobody.start()
-                    self.emit_state_changed(State.STATE_FACE_NOT_WORKING, self.UNKNOWN)
+                    self.nobodyTimer.start(self.nobodyTimerInterval)
 
+        # Case an face is present but we don't know who is it
         elif id == self.UNKNOWN:
             self.pretendent_id = self.NOBODY
-            self.counter_nobody.stop()
+            self.nobodyTimer.stop()
             self.counter_known.stop()
 
-            if not self.counter_unknown.is_active():
+            if not self.counter_unknown.isActive():
                 if self.state_id != self.UNKNOWN:
-                    self.counter_unknown.start()
+                    self.counter_unknown.start(self.unknownTimerInterval)
                     self.emit_state_changed(State.STATE_FACE_WORKING, self.UNKNOWN)
 
+        # Case an know person is present
         else:
-            self.counter_nobody.stop()
+            self.nobodyTimer.stop()
             self.counter_unknown.stop()
 
             if id == self.state_id:
@@ -126,11 +133,11 @@ class FaceRecognition(QThread):
             else:
                 if id != self.pretendent_id:
                     self.pretendent_id = id
-                    self.counter_known.start()
+                    self.counter_known.start(self.knownTimerInterval)
                     self.emit_state_changed(State.STATE_FACE_WORKING, self.UNKNOWN)
 
     @pyqtSlot()
-    def timer_nobody_timeout(self):
+    def nobodyTimeout(self):
         self.state_id = self.NOBODY
         self.emit_person_changed(self.state_id)
         self.emit_state_changed(State.STATE_FACE_NOBODY, self.NOBODY)
@@ -167,7 +174,7 @@ class FaceRecognition(QThread):
     def emit_person_changed(self, id):
         name = self.id_to_name(id)
         print(name)
-        self.person_changed.emit(name)
+        self.signalPersonChanged.emit(name)
 
     def add_picture(self, picture):
         if picture.user.id in self.data:

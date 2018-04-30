52a87834fd1f04fb0a2df7a289052c920a7649a0
Alain Berrier <alain.berrier@outlook.com>
2018-04-30 17:32:58 +0200

    revert audio

--- surirobot/services/audioplayer.py
+++ surirobot/services/audioplayer.py
@@ -1,9 +1,8 @@
 from PyQt5.QtCore import QThread, pyqtSlot
 import logging
-import sys
-import platform
+import simpleaudio as sa
 import subprocess
-from playsound import playsound
+
 
 class AudioPlayer(QThread):
     def __init__(self):
@@ -14,7 +13,14 @@ class AudioPlayer(QThread):
         self.playObj = None
 
     def __del__(self):
+        self.stop()
         self.quit()
+
+    @pyqtSlot()
+    def stop(self):
+        if self.playObj:
+            self.playObj.stop()
+            self.logger.info('Stop playing.')
 
     @pyqtSlot(str)
     def play(self, filename):
@@ -23,6 +29,9 @@ class AudioPlayer(QThread):
             if platform.system() == "Darwin":
                 subprocess.call(["afplay", filename])
             else:
-                playsound(filename)
+                self.stop()
+                waveObj = sa.WaveObject.from_wave_file(filename)
+                self.logger.info('Now playing' + str(filename) + '.')
+                self.playObj = waveObj.play()
         except Exception as e:
             self.logger.info('Error : .' + str(e))

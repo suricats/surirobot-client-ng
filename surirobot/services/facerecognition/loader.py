eeaee911b06d93810ddcd8e36d080fedf013e6e7
Alain BERRIER <alain.berrier@outlook.com>
2018-06-23 18:29:06 +0200

    fix bug adding picture

--- surirobot/services/facerecognition/loader.py
+++ surirobot/services/facerecognition/loader.py
@@ -3,17 +3,19 @@ import logging
 import queue
 import uuid
 import shutil
-from surirobot.management import db
-from surirobot.management.mod_api.models import User, Picture
 from surirobot.services import serv_fr, serv_vc
 from surirobot.core.common import Dir
 import cv2
 import os
 import requests
 
+
 class FaceLoader(QThread):
     signalIndicator = pyqtSignal(str, str)
     new_user = pyqtSignal(str)
+    token = os.environ.get('API_MEMORY_TOKEN', '')
+    url = os.environ.get('API_MEMORY_URL', '')
+    headers = {'Authorization': 'Token ' + token}
 
     def __init__(self):
         QThread.__init__(self)
@@ -27,23 +29,19 @@ class FaceLoader(QThread):
         self.quit()
 
     def run(self):
-        self.load_from_external_db()
-        #self.load_from_db()
+        self.load_from_db()
 
         while True:
             picture = self.q.get()
             serv_fr.addPicture(picture)
 
-    def load_from_external_db(self):
+    def load_from_db(self):
         try:
             self.logger.info("Start loading faces ....")
-            token = os.environ.get('API_MEMORY_TOKEN', '')
-            url = os.environ.get('API_MEMORY_URL', '')
-            headers = {'Authorization': 'Token ' + token}
-            r1 = requests.get(url + '/memorize/users/', headers=headers)
+            r1 = requests.get(self.url + '/memorize/users/', headers=self.headers)
             users = r1.json()["results"]
             for user in users:
-                r2 = requests.get(url + '/memorize/users/' + str(user.get("id")) + '/pictures', headers=headers)
+                r2 = requests.get(self.url + '/memorize/users/' + str(user.get("id")) + '/pictures', headers=self.headers)
                 pictures = r2.json()
                 if pictures:
                     picture = pictures[0]
@@ -55,38 +53,24 @@ class FaceLoader(QThread):
         except Exception as e:
             print("load from db : " + str(e))
 
-    def load_from_db(self):
-        self.logger.info("Start loading faces ....")
-        users = User.query.all()
-
-        for user in users:
-            pictures = user.pictures
-            if pictures:
-                picture = pictures[0]
-                # name = user.firstname + ' ' + user.lastname
-                # self.logger.info("Load Face {}".format(name))
-                serv_fr.addPicture(picture)
-                self.signalIndicator.emit("face", "orange")
-        self.signalIndicator.emit("face", "green")
-        self.logger.info('Loaded all the faces from DB')
-
     def add_user(self, firstname, lastname, email, picture_path):
-        user = User(firstname=firstname, lastname=lastname, email=email)
-        db.session.add(user)
-        db.session.commit()
+        r1 = requests.post(self.url + '/memorize/users/', {"firstname": firstname, "lastname": lastname, "email": email}, headers=self.headers)
+        user = r1.json()
         new_path = Dir.PICTURES + format(uuid.uuid4()) + '.jpg'
         shutil.copy(picture_path, new_path)
-        print(user.id)
-        picture = Picture(path=new_path, user=user)
-        db.session.add(picture)
-        db.session.commit()
-
+        r2 = requests.post(self.url + '/memorize/pictures/', {"path": new_path, "user_id": user["id"]}, headers=self.headers)
+        picture = r2.json()
+        picture['user'] = user
         self.q.put(picture)
 
     @pyqtSlot(str, str)
     def take_picture_new_user(self, firstname, lastname):
-        picture = serv_vc.get_frame()
-        file_path = Dir.TMP + format(uuid.uuid4()) + '.jpeg'
-        cv2.imwrite(file_path, picture)
-        self.new_user.emit(firstname)
-        self.add_user(firstname, lastname, '', file_path)
+        try:
+            print("take_picture_new_user")
+            picture = serv_vc.get_frame()
+            file_path = Dir.TMP + format(uuid.uuid4()) + '.jpeg'
+            cv2.imwrite(file_path, picture)
+            self.new_user.emit(firstname)
+            self.add_user(firstname, lastname, '', file_path)
+        except Exception as e:
+            print(e)

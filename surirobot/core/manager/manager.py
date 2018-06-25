aedc9704c90bc1513d606c43520c9510e3c8b531
Alain BERRIER <alain.berrier@outlook.com>
2018-06-25 16:22:49 +0200

    macos debug

--- surirobot/core/manager/manager.py
+++ surirobot/core/manager/manager.py
@@ -223,7 +223,9 @@ class Manager(QObject):
                 self.scopeChanged = False
         except Exception as e:
             print("Error - checkScope")
-            print(e)
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(e).__name__, e.args)
+            print(message)
 
     def updateState(self, sc):
         for trigger in sc["triggers"]:

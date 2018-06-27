b252ff009d1cb85ddb162c0af6ce9dfb38ae17c2
Alain BERRIER <alain.berrier@outlook.com>
2018-06-27 16:05:59 +0200

    emotion api call shielded

--- surirobot/services/emotional/emotional.py
+++ surirobot/services/emotional/emotional.py
@@ -49,8 +49,7 @@ class EmotionalRecognition(QThread):
                         file_path = Dir.TMP + format(uuid.uuid4()) + '.jpeg'
                         cv2.imwrite(file_path, frame)
                         img = face_recognition.load_image_file(file_path)
-                        encodings = True
-                        # encodings = face_recognition.face_encodings(img, None, 10)
+                        encodings = face_recognition.face_encodings(img, None, 10)
                         if encodings:
                             self.isBusy = True
                             self.send_request.emit(file_path)

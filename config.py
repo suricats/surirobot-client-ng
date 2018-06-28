1c79323ee15376255f64c930e51da8377165e042
Alain BERRIER <alain.berrier@outlook.com>
2018-06-28 13:31:00 +0200

    cleaning env file

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_config():
-    return dict(
-        DEBUG=True,
-        SQLALCHEMY_DATABASE_URI=os.environ.get('SURIROBOT_DB'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False
-    )

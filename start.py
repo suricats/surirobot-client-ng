6f479c1a426fad312a7cc73e38ed67d267d94b35
Alain BERRIER <alain.berrier@outlook.com>
2018-07-16 12:08:37 +0200

    addind local tts

--- start.py
+++ start.py
@@ -11,7 +11,8 @@ import surirobot.core.keyboard
 
 import surirobot.core.api
 from surirobot.core.manager import manager
-if not os.environ.get('TEMPORARY_FILES', True):
+if not int(os.environ.get('TEMPORARY_FILES', '0')):
+    print('banana')
     app.aboutToQuit.connect(manager.delete_temporary_files)
 app.exec_()
 # Launch Flask

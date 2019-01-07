85b11b42a25c8a6f8033ca7ba30b4c768a9a969e
abrunberaud@gmail.com <>
2019-01-07 14:11:02 +0100

    WIP: Imp Tests & coverage for APICaller

---
+++ surirobot/core/api/test_apiCaller.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class TestApiCaller(TestCase):
+    def test_start(self):
+        self.assertTrue(self) == True
+
+    def test_stop(self):
+        self.assertTrue(self) == True

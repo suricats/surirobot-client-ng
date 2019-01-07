f93718b4e0b90744a01d31c8e39210c7453f17b9
abrunberaud@gmail.com <>
2019-01-07 15:00:07 +0100

    WIP: Imp Tests & coverage for MemoryAPI

---
+++ surirobot/core/api/test_memoryApiCaller.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestMemoryApiCaller(TestCase):
+    def test_get_encodings(self):
+        self.setUp()
+
+    def test_get_notifications(self):
+        self.setUp()
+
+    def test_get_users(self):
+        self.setUp()
+
+    def test_get_user(self):
+        self.setUp()
+
+    def test_get_last_sensor(self):
+        self.setUp()
+
+    def test_get_sensors(self):
+        self.setUp()
+
+    def test_add_user(self):
+        self.setUp()
+
+    def test_add_encoding(self):
+        self.setUp()

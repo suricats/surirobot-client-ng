518c319bdd90ab0e0ea75cd64b1c842265df039e
abrunberaud@gmail.com <>
2019-01-07 14:00:52 +0100

    WIP: Imp Tests & coverage for Emotional Analysis

--- surirobot/core/api/test_emotionalAPICaller.py
+++ surirobot/core/api/test_emotionalAPICaller.py
@@ -3,10 +3,15 @@ from unittest import TestCase
 
 class TestEmotionalAPICaller(TestCase):
     def test_analyse(self):
-        self.setUp()
+        self.file_path = "str"
+        self.integer_file_path = 123
+        self.assertEqual(self.file_path, "str", "Correct file path")
+        self.assertNotEqual(self.integer_file_path, "str", "Wrong file path")
+        self.assertTrue(self.file_path)
 
     def test_getAnalysis(self):
-        self.setUp()
+        self.file_path= "string"
+        self.assertEqual(self.file_path, "string", "Correct")
 
     def test_start(self):
         self.setUp()

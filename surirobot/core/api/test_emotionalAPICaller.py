9cda68adbedc2eeac0dfa4064b117d0e3c4cb9ff
abrunberaud@gmail.com <>
2019-01-07 13:35:47 +0100

    WIP: Add Tests & coverage

---
+++ surirobot/core/api/test_emotionalAPICaller.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestEmotionalAPICaller(TestCase):
+    def test_analyse(self):
+        self.setUp()
+
+    def test_getAnalysis(self):
+        self.setUp()
+
+    def test_start(self):
+        self.setUp()

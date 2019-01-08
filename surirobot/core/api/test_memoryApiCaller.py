a4a52b73384829f3ff21e2c89e54233d4c3aca7f
abrunberaud@gmail.com <>
2019-01-08 12:11:26 +0100

    WIP: Test adding new user & Imp syntax

--- surirobot/core/api/test_memoryApiCaller.py
+++ surirobot/core/api/test_memoryApiCaller.py
@@ -23,22 +23,32 @@ class TestMemoryApiCaller(TestCase):
     def test_get_notifications(self):
         token = '4d060171dce2fb7add839c93e9b1d10509a52257'
         self.headers = {'Authorization': 'Token ' + token, 'Content-Type': 'application/json'}
-        response_encoding = requests.get('https://memory.api.suricats-consulting.com/api/notifications/',
+        response_notifications = requests.get('https://memory.api.suricats-consulting.com/api/notifications/',
                                          headers=self.headers)
-        self.assertEqual(response_encoding.status_code, 200)
+        self.assertEqual(response_notifications.status_code, 200)
 
     def test_get_last_sensor(self):
         token = '4d060171dce2fb7add839c93e9b1d10509a52257'
         self.headers = {'Authorization': 'Token ' + token, 'Content-Type': 'application/json'}
-        response_encoding = requests.get('https://memory.api.suricats-consulting.com/api/memory/sensors/last',
+        response_last_sensor = requests.get('https://memory.api.suricats-consulting.com/api/memory/sensors/last',
                                          headers=self.headers)
-        self.assertEqual(response_encoding.status_code, 200)
+        self.assertEqual(response_last_sensor.status_code, 200)
 
     def test_get_sensors(self):
-        self.setUp()
+        token = '4d060171dce2fb7add839c93e9b1d10509a52257'
+        self.headers = {'Authorization': 'Token ' + token, 'Content-Type': 'application/json'}
+        response_sensor = requests.get('https://memory.api.suricats-consulting.com/api/memory/sensors/',
+                                         headers=self.headers)
+        self.assertEqual(response_sensor.status_code, 200)
 
     def test_add_user(self):
-        self.setUp()
+        data = {"firstname": 'firstname', "lastname": 'lastname', "email": 'toto.test@suricats-consulting.com'}
+        token = '4d060171dce2fb7add839c93e9b1d10509a52257'
+        self.headers = {'Authorization': 'Token ' + token, 'Content-Type': 'application/json'}
+        response_add_user = requests.get('https://memory.api.suricats-consulting.com/api/memory/users/',
+                                         json=data,
+                                         headers=self.headers)
+        self.assertEqual(response_add_user.status_code, 200)
 
     def test_add_encoding(self):
         self.setUp()

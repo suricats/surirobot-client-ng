dd903efdbadca2872e5114cee052b5620872f76d
abrunberaud@gmail.com <>
2019-01-08 11:24:03 +0100

    WIP: Imp unittests (7/9) for MemoryAPI

--- surirobot/core/api/test_memoryApiCaller.py
+++ surirobot/core/api/test_memoryApiCaller.py
@@ -8,18 +8,24 @@ class TestMemoryApiCaller(TestCase):
     def test_get_users(self):
         token = '4d060171dce2fb7add839c93e9b1d10509a52257'
         self.headers = {'Authorization': 'Token ' + token, 'Content-Type': 'application/json'}
-        response_users = requests.get('http://memory.api.suricats-consulting.com/api/memory/users/ ',
+        self.response_users = requests.get('http://memory.api.suricats-consulting.com/api/memory/users/',
                                       headers=self.headers)
         # Receive response
-        self.assertEqual(response_users.status_code, 200)
+        self.assertEqual(self.response_users.status_code, 200)
 
     def test_get_encodings(self):
-        response_encoding = requests.get('https://converse.api.suricats-consulting.com/api/memory/users/')
+        token = '4d060171dce2fb7add839c93e9b1d10509a52257'
+        self.headers = {'Authorization': 'Token ' + token, 'Content-Type': 'application/json'}
+        response_encoding = requests.get('https://memory.api.suricats-consulting.com/api/memory/encodings/',
+                                         headers=self.headers)
         self.assertEqual(response_encoding.status_code, 200)
 
     def test_get_notifications(self):
-        response = requests.get('https://converse.api.suricats-consulting.com/api/notifications')
-        self.assertEqual(response.status_code, 200)
+        token = '4d060171dce2fb7add839c93e9b1d10509a52257'
+        self.headers = {'Authorization': 'Token ' + token, 'Content-Type': 'application/json'}
+        response_encoding = requests.get('https://memory.api.suricats-consulting.com/api/notifications/',
+                                         headers=self.headers)
+        self.assertEqual(response_encoding.status_code, 200)
 
     def test_get_last_sensor(self):
         self.setUp()

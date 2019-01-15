132067545af61748e283aab4c06490618b6fdf95
abrunberaud@gmail.com <>
2019-01-15 14:43:07 +0100

    WIP: Add Test for Emotional

--- surirobot/core/api/test_emotionalAPICaller.py
+++ surirobot/core/api/test_emotionalAPICaller.py
@@ -11,8 +11,18 @@ class TestEmotionalAPICaller(TestCase):
         self.assertEqual(response_analysis.status_code, 200)
 
     def test_getAnalysis(self):
+
+        # Test token verification
         res = requests.post("https://token.beyondverbal.com/token", data={"grant_type": "client_credentials",
-                                                                          "apiKey": '76918380-3268-40be-8dc6-fba59804dd76'})
+                                         "apiKey": '76918380-3268-40be-8dc6-fba59804dd76'})
         self.assertEqual(res.status_code, 200)
 
+        # Test the sample analysis
+        token = res.json()['access_token']
+        headers = {"Authorization": "Bearer " + token}
+        pp = requests.post("https://apiv4.beyondverbal.com/v4/recording/start",
+                           json={"dataFormat": {"type": "WAV"}},
+                           verify=False,
+                           headers=headers)
 
+        self.assertEqual(pp.status_code, 200)
